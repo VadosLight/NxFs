f03adb2e2ea245721e05cf78fc819af6d56ec8cb
Vadim Tatarincev <it-vadim@inbox.ru>
2022-05-28 10:35:57 +0300

    коммит не должен пройти в дев

--- apps/front/src/app/app.tsx
+++ apps/front/src/app/app.tsx
@@ -3,6 +3,7 @@ import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  // коммит не должен пройти в дев
   return (
     <>
       <NxWelcome title="front" />
